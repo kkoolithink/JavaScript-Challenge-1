b84ef8208fc11cc4ed9ee64f73fee24c7a7bb375
Kenneth <kensotomartinez@gmail.com>
2025-03-31 13:03:35 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("Welcome to my JavaScript Page!");
+console.log("JavaScript makes webpages interactive!");
+
+alert("Hello! Welcome to my webpage.");
+
+document.querySelector("#title").textContent = "What's up?";
+document.querySelector("#message").textContent = "I love coding!";
